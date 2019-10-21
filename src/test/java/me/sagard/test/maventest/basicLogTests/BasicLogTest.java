4cf7fab2b34ce35cc64b41d0934e64f59d86a9a8
Sagar D <sagard@practo.com>
2019-10-21 15:48:28 +0530

    Update Log messages

--- src/test/java/me/sagard/test/maventest/basicLogTests/BasicLogTest.java
+++ src/test/java/me/sagard/test/maventest/basicLogTests/BasicLogTest.java
@@ -9,16 +9,18 @@ public class BasicLogTest extends BaseClass{
 	
 	@Test(groups = {"logTest"})
 	public static void testLetsPrintHello() {
-		System.out.println("HELLo");
+		System.out.println("Log Test 1");
 	}
 	
 	@Test(groups = {"logTest", "failTest"})
 	public static void failATest() {
+		System.out.println("Log Test 2");
 		Assert.assertTrue(false);
 	}
 	
 	@Test(groups = {"logTest", "passTest"})
 	public static void passATest() {
+		System.out.println("Log Test 3");
 		Assert.assertTrue(true);
 	}
 	
